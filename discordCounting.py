f4c0ecaec678e776f0c44970750e3715491d4ff5
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2023-10-15 11:29:37 +0700

    Discord counting

--- discordCounting.py
+++ discordCounting.py
@@ -1,14 +1,13 @@
 import pyautogui
 import time
-import random
+from random import randint
 
 pyautogui.hotkey('win', '1')
 
 x = 1
 while x < 100:
-    time.sleep(1);
+    time.sleep(randint(5, 20) / 10);
     pyautogui.hotkey('ctrl', str(x % 2 + 1))
-    pyautogui.write(str(x), interval = random.randint(0, 15) / 10)
+    pyautogui.write(str(x), interval = randint(0, 15) / 10)
     pyautogui.press('enter');
     x += 1;
-
