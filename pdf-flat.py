d75bb78d6edf5967361fabd019a8b604c1b8c5b6
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2026-02-17 13:43:55 -0800

    Put your own file name

--- pdf-flat.py
+++ pdf-flat.py
@@ -2,12 +2,9 @@ import os, pymupdf, img2pdf
 
 # list all files from windows download directory
 downloads_folder = os.path.join(os.path.expanduser("~"), "Downloads")
-files = os.listdir(downloads_folder)
-files = filter(lambda f: f.endswith("Project.pdf"), files)
-files = filter(lambda f: f.startswith("[Physics 12] U"), files)
-files = sorted(files)
+name = '[Chemistry 11] U4 Project.pdf'
 
-doc = pymupdf.open(os.path.join(downloads_folder, files[-1]))
+doc = pymupdf.open(os.path.join(downloads_folder, name))
 for page in doc:
     pix = page.get_pixmap(
         dpi=150,
@@ -15,12 +12,9 @@ for page in doc:
     )
     pix.save("pdf-flat/page-%i.png" % page.number)
 
-    if page.number == 7:
-        break
-
-with open(os.path.join(downloads_folder, files[-1][:-4] + "-flat.pdf"), "wb") as f:
+with open(os.path.join(downloads_folder, name[:-4] + "-flat.pdf"), "wb") as f:
     f.write(img2pdf.convert(
-        ["pdf-flat/page-%i.png" % i for i in range(0, 7)]
+        ["pdf-flat/page-%i.png" % i for i in range(0, len(doc))]
     ))
 
 for i in range(len(doc)):
